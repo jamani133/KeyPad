26bcbf86a7e48a835dc8902656e6c336d422a0e7
jamani133 <68377235+jamani133@users.noreply.github.com>
2024-09-20 12:57:03 +0200

    aaa

--- Hardware/Hardware.ino
+++ Hardware/Hardware.ino
@@ -22,8 +22,10 @@ bool       prevKeyStates[7] = {  false,  false,  false,  false,  false,  false, 
 uint16_t     changePoint[6] = {      0,      0,      0,      0,      0,      0 };
 uint16_t         topTrim[6] = {      0,      0,      0,      0,      0,      0 };
 uint16_t      bottomTrim[6] = {      0,      0,      0,      0,      0,      0 };
-char                keys[7] = {    'x',    'c',    'v',    'b',    'd',    'f',    'g'};
 
+uint16_t             pos[6] = {      0,      0,      0,      0,      0,      0 };
+
+uint8_t mode = 0;
 AS5600 as5600;   //  use default Wire
 int prevAngle = 0;
 int angle = 0;
@@ -31,54 +33,37 @@ int spinnerSpeed = 0;
 CRGBArray<LED_COUNT> leds;
 uint8_t rainbow = 0;
 bool calibrate = false;
+bool ledState = false;
 void setup(){
     Serial.begin(9600);
+    
     Wire.begin();
     Consumer.begin();
     as5600.begin();
     loadSettings();
     FastLED.addLeds<NEOPIXEL,LED_PIN>(leds, LED_COUNT);
     pinMode(pins[6],INPUT_PULLUP);
+    Gamepad.begin();
     delay(1000);
+    
 }
 void loop(){
     EVERY_N_MILLISECONDS( 10 ) {
         handleSpinner();
     }
     EVERY_N_MILLISECONDS( 25 ) {
-        updateLeds();
+        if(ledState){
+            updateLeds();
+        }
         if(calibrate){
             calib();
         }
     }
-
-
     EVERY_N_MILLISECONDS( 1000 ) {
-        if(Serial.available()){
-            String cmd = Serial.readString();
-            if (cmd.equals("save\n")){
-                Serial.println("Calibration Saved");
-                saveSettings();
-                calibrate = false;
-            }
-            if (cmd.equals("calibrate\n")){
-                Serial.println("Calibration Started");
-                calibrate = true;
-                resetCalibration();
-            }
-            if (cmd.equals("stop\n")){
-                Serial.println("Calibration Stopped");
-                calibrate = false;
-            }
-            if (cmd.equals("load\n")){
-                Serial.println("Calibration Loaded");
-                calibrate = false;
-                loadSettings();
-            }
-        }
+        getSerial();
     }
 
-    delayMicroseconds(500);
+    //delayMicroseconds(500);
     readButtons();
     sendEvents();
     for(int i = 0; i<7;i++){
@@ -86,9 +71,110 @@ void loop(){
     }
     
 }
-
+void getSerial(){
+    if(Serial.available()){
+        String cmd = Serial.readString();
+        if (cmd.equals("save\n")){
+            Serial.println("Calibration Saved");
+            saveSettings();
+            calibrate = false;
+        }
+        if (cmd.equals("calibrate\n")){
+            Serial.println("Calibration Started");
+            calibrate = true;
+            resetCalibration();
+        }
+        if (cmd.equals("stop\n")){
+            Serial.println("Calibration Stopped");
+            calibrate = false;
+        }
+        if (cmd.equals("load\n")){
+            Serial.println("Calibration Loaded");
+            calibrate = false;
+            loadSettings();
+        }
+        if (cmd.equals("led_on\n")){
+            Serial.println("led on");
+            
+            ledState = true;
+        }
+        if (cmd.equals("led_off\n")){
+            Serial.println("led off");
+            
+            ledState = false;
+        }
+        if (cmd.equals("osu\n")){
+            Serial.println("Reboot to switch to osu! Mode");
+            mode = 0;
+            ledState = true;
+        }
+        if (cmd.equals("zeep\n")){
+            Serial.println("Reboot to switch to Zeepkist Mode");
+            mode = 1;
+        }
+        if (cmd.equals("controller\n")){
+            Serial.println("Reboot to switch to controller Mode");
+            mode = 2;
+        }
+        if (cmd.equals("all\n")){
+            Serial.println("Reboot to switch to all inputs Mode");
+            mode = 3;
+        }
+        if (cmd.equals("help\n")){
+            Serial.print("\n\nPossible commands :\n----------------------------------------------\n");
+            Serial.print("          save : save settings to EEPROM\n");
+            Serial.print("     calibrate : calibrate analog keys\n");
+            Serial.print("          stop : stop calibrating\n");
+            Serial.print("          save : stop and save calibration\n");
+            Serial.print("          load : load settings from EEPROM\n");
+            Serial.print("led_on/led_off : turn leds on / off\n");
+            Serial.print("           osu : key config for osu!\n");
+            Serial.print("          zeep : key config for Zeepkist\n");
+            Serial.print("    controller : analog controller loayout\n");
+            Serial.print("           all : all buttons mapped to keys\n\n");
+        }
+    }
+}
 
 void sendEvents(){
+    switch(mode){
+        case 0:
+            sendOSU();
+        break;
+        case 1:
+            sendZEEP();
+        break;
+        case 2:
+            sendCTRL();
+        break;
+        case 3:
+            sendALL();
+        break;
+    }
+}
+void sendOSU(){
+    char                keys[2] = {    'x',    'c'};
+    for(int i = 0; i<2;i++){
+        if(keyStates[i] && !prevKeyStates[i]){
+            Keyboard.press(keys[i]);
+        }
+        if(!keyStates[i] && prevKeyStates[i]){
+            Keyboard.release(keys[i]);
+        }
+    }
+    if(pos[3] > 20){
+        keyboard.write(KEY_ENTER);
+    }
+    if(pos[5] > 20){
+        keyboard.write(KEY_ESC);
+    }
+    if(pos[6] > 20){
+        keyboard.write(KEY_F2);
+    }
+
+}
+void sendALL(){
+    char                keys[7] = {    'a',    's',    'd',    'f',    'w',    'e',    'r'};
     for(int i = 0; i<7;i++){
         if(keyStates[i] && !prevKeyStates[i]){
             Keyboard.press(keys[i]);
@@ -98,6 +184,46 @@ void sendEvents(){
         }
     }
 }
+void sendZEEP(){
+    //char                keys[7] = {    'AL',    'DOWN',    'AR',    '',    'UP',    '',    'RSHIFT'};
+    
+    if(keyStates[1] && !prevKeyStates[1]){
+        Keyboard.press(KEY_DOWN);
+    }
+    if(!keyStates[1] && prevKeyStates[1]){
+        Keyboard.release(KEY_DOWN);
+    }
+
+    if(keyStates[4] && !prevKeyStates[4]){
+        Keyboard.press(KEY_UP);
+    }
+    if(!keyStates[4] && prevKeyStates[4]){
+        Keyboard.release(KEY_UP);
+    }
+
+    if(pos[6] > 20){
+        keyboard.write(KEY_RIGHT_SHIFT);
+    }
+    
+
+    Gamepad.xAxis(analogToAxis(pos[0],pos[2]));
+    
+}
+void sendCTRL(){
+    Gamepad.yAxis(analogToAxis(pos[2],pos[5]));
+    Gamepad.xAxis(analogToAxis(pos[1],pos[3]));
+    Gamepad.zAxis(analogToAxis(pos[0],pos[4]));
+}
+
+void analogToAxis(int neg, int pos){
+    if(neg <= AnalogDeadzone){
+        neg = 0;
+    }
+    if(pos <= AnalogDeadzone){
+        pos = 0;
+    }
+    return map(pos-neg,-28,28,0,0xFFFF);
+}
 
 void calib(){
     for(int i = 0; i<6;i++){
@@ -105,11 +231,11 @@ void calib(){
         for(int j = 0;j<20;j++){
             analogVal += analogRead(pins[i]);
         }
-        uint16_t pos = analogVal/20;
-        if(pos < bottomTrim[i]){
-            bottomTrim[i] = pos;
-        }else if(pos > topTrim[i]){
-            topTrim[i] = pos;
+        pos[i] = analogVal/20;
+        if(pos[i] < bottomTrim[i]){
+            bottomTrim[i] = pos[i];
+        }else if(pos[i] > topTrim[i]){
+            topTrim[i] = pos[i];
         }
     }
 }
@@ -126,19 +252,19 @@ void resetCalibration(){
 void readButtons(){
     for(int i = 0; i<6;i++){
         int analogVal = 0;
-        for(int j = 0;j<5;j++){
+        for(int j = 0;j<16;j++){
             analogVal += analogRead(pins[i]);
         }
-        uint16_t pos = sqrt(map(analogVal/5,bottomTrim[i],topTrim[i],1024,0));
-        if(pos < deadzone){
+        pos[i] = sqrt(map(analogVal>>4,bottomTrim[i],topTrim[i],1024,0));
+        if(pos[i] < deadzone){
             keyStates[i] = false;
         }else{
-            if(changePoint[i] < pos){
-                changePoint[i] = pos;
+            if(changePoint[i] < pos[i]){
+                changePoint[i] = pos[i];
                 keyStates[i] = true;
             }
-            if(changePoint[i] > pos+RTDeadzone){
-                changePoint[i] = pos+RTDeadzone;
+            if(changePoint[i] > pos[i]+RTDeadzone){
+                changePoint[i] = pos[i]+RTDeadzone;
                 keyStates[i] = false;
             }
         }
@@ -156,6 +282,8 @@ void loadSettings(){
         topTrim[i]     = EEPROM.read(2*i)|EEPROM.read(2*i+1)<<8;
         bottomTrim[i]  = EEPROM.read((2*i)+12)|EEPROM.read(2*i+13)<<8;
     }
+    ledState = EEPROM.read(68);
+    mode = EEPROM.read(69);
 }
 
 void saveSettings(){
@@ -172,6 +300,8 @@ void saveSettings(){
         Serial.print(topTrim[i]);
         Serial.print("\n");
     }
+    EEPROM.write(68,ledState);
+    EEPROM.write(69,mode);
 }
 
 void handleSpinner(){
@@ -193,12 +323,36 @@ void handleSpinner(){
 }
 
 void updateLeds(){
-    for(int i = 0; i<7;i++){
-        if(keyStates[i]){
-            leds[i] = CRGB(127,64,0);
-        }else{
-            leds[i] = CRGB(0,64,0);
-        }
+    swtich(mode){
+        case 0:
+            if(keyStates[0]){leds[0] = CRGB(128,64,0);}else{leds[0] = CRGB(0,0,64);}
+            if(keyStates[1]){leds[1] = CRGB(128,64,0);}else{leds[1] = CRGB(0,0,64);}
+
+            if(keyStates[3]){leds[3] = CRGB(128,64,0);}else{leds[3] = CRGB(0,64,0);}
+            if(keyStates[5]){leds[5] = CRGB(128,64,0);}else{leds[5] = CRGB(64,0,0);}
+            if(keyStates[6]){leds[6] = CRGB(128,64,0);}else{leds[6] = CRGB(0,64,0);}
+        break;
+
+        case 1:
+            leds[0] = CRGB(map(pos[0],0,28,0,128),64,0);
+            leds[2] = CRGB(map(pos[2],0,28,0,128),64,0);
+            if(keyStates[1]){leds[1] = CRGB(128,64,0);}else{leds[1] = CRGB(0,0,64);}
+            if(keyStates[4]){leds[4] = CRGB(128,64,0);}else{leds[4] = CRGB(0,0,64);}
+            if(keyStates[6]){leds[6] = CRGB(128,64,0);}else{leds[6] = CRGB(64,0,0);}
+        break;
+
+        case 2:
+            for(int i = 0;i<6;i++){
+                leds[i] = CRGB(map(pos[i],0,28,0,128),64,0);
+            }
+            if(keyStates[6]){leds[6] = CRGB(128,64,0);}else{leds[6] = CRGB(0,64,0);}
+        break;
+
+        case 3:
+            for(int i = 0;i<7;i++){
+                if(keyStates[i]){leds[i] = CRGB(128,64,0);}else{leds[i] = CRGB(0,64,0);}
+            }
+        break;
     }
     for(int i = 0; i<4;i++){
         leds[i+7] = CHSV(rainbow+(64*i),255,64);
